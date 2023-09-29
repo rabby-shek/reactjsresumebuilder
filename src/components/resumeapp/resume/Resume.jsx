06a867e41454ca4269ae2a6cc58416edde40a00d
rabby-shek <rabbyshek1998@gmail.com>
2023-09-29 23:24:37 +0600

    mdb packede adde topnav adde in the app

---
+++ src/components/resumeapp/resume/Resume.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Resume = () => {
+  return (
+    <div className='resume-container'>
+      this is the resume component
+    </div>
+  )
+}
+
+export default Resume
--- src/layouts/AppLayouts.jsx
+++ src/layouts/AppLayouts.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Resume from '../components/resumeapp/resume/Resume'
+import TopNav from '../components/resumeapp/topnav/TopNav'
 
 const AppLayouts = () => {
   return (
     <div>
-      this is app layout
+      <TopNav />
+      <Resume />
     </div>
   )
 }
