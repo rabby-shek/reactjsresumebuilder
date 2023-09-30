8f4fc7dfaf7654ec217a18cd2f628bc52b2a373f
rabby-shek <rabbyshek1998@gmail.com>
2023-09-30 22:10:58 +0600

    topnav updated: cardt icon deleted

--- src/components/resumeapp/topnav/TopNav.jsx
+++ src/components/resumeapp/topnav/TopNav.jsx
@@ -46,46 +46,7 @@ const TopNav = () => {
         {/* Collapsible wrapper */}
         {/* Right elements */}
         <div className="d-flex align-items-center">
-          {/* Icon */}
-          <a className="text-reset me-3" href="#">
-            <i className="fas fa-shopping-cart" />
-          </a>
-          {/* Notifications */}
-          <div className="dropdown">
-            <a
-              className="text-reset me-3 dropdown-toggle hidden-arrow"
-              href="#"
-              id="navbarDropdownMenuLink"
-              role="button"
-              data-mdb-toggle="dropdown"
-              aria-expanded="false"
-            >
-              <i className="fas fa-bell" />
-              <span className="badge rounded-pill badge-notification bg-danger">
-                1
-              </span>
-            </a>
-            <ul
-              className="dropdown-menu dropdown-menu-end"
-              aria-labelledby="navbarDropdownMenuLink"
-            >
-              <li>
-                <a className="dropdown-item" href="#">
-                  Some news
-                </a>
-              </li>
-              <li>
-                <a className="dropdown-item" href="#">
-                  Another news
-                </a>
-              </li>
-              <li>
-                <a className="dropdown-item" href="#">
-                  Something else here
-                </a>
-              </li>
-            </ul>
-          </div>
+      
           {/* Avatar */}
           <div className="dropdown">
             <a
